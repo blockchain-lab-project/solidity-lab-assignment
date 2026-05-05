f5407ddb5750283f78b841e94bc393757f85181f
bethel-k-97 <bettyk2197@gmail.com>
2026-05-05 21:17:20 +0300

    calling contracts

--- Code/Address Interaction/Calling contracts/Arbitrary Alert.sol
+++ Code/Address Interaction/Calling contracts/Arbitrary Alert.sol
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 contract Sidekick {
-    function relay(address hero, bytes memory data) external {
-        // send all of the data as calldata to the hero
+
+    function relay(address hero, bytes calldata data) external {
         (bool success, ) = hero.call(data);
-    require(success);
+        require(success);
     }
 }
