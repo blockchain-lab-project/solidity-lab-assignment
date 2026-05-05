f2200e0b2ef8b9f468b6ec34619d8c95ee705a0a
bethel-k-97 <bettyk2197@gmail.com>
2026-05-05 21:12:46 +0300

    approval.sol

--- Code/Address Interaction/Escrow/Approval.sol
+++ Code/Address Interaction/Escrow/Approval.sol
@@ -1,17 +1,19 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 contract Escrow {
+
     address public depositor;
     address public beneficiary;
     address public arbiter;
 
     constructor(address _arbiter, address _beneficiary) payable {
-        depositor = msg.sender;
         arbiter = _arbiter;
         beneficiary = _beneficiary;
+        depositor = msg.sender;
     }
+
     function approve() external {
-    payable(beneficiary).transfer(address(this).balance);
-}
+        (bool success, ) = beneficiary.call{value: address(this).balance}("");
+        require(success);
+    }
 }
